92ef2e96da4d2e465a425da884598a9ba9f46917
crgimenes <crg@crg.eti.br>
2022-09-26 11:13:09 -0300

    add shutdown

--- M5Stack/fishing-rod/fishing-rod.ino
+++ M5Stack/fishing-rod/fishing-rod.ino
@@ -2,9 +2,15 @@
 
 
 void setup() {
-
+  M5.begin();
+  M5.Power.begin();
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.print("starting...\n");
 }
 
 void loop() {
-
+  M5.update();
+  if (M5.BtnA.pressedFor(1000, 200)) { // If A is pressed for 1 second power off
+    M5.Power.powerOFF();
+  }
 }
