468d645aa262838f0958d3334ad5d07a0dfc88ed
crgimenes <crg@crg.eti.br>
2022-09-27 09:15:41 -0300

    improve shutdown

--- M5Stack/fishing-rod/fishing-rod.ino
+++ M5Stack/fishing-rod/fishing-rod.ino
@@ -12,22 +12,38 @@ void connectWiFi() {
   }
 }
 
-
-
 void setup() {
   M5.begin();
   M5.Power.begin();
-  M5.Lcd.setTextSize(3);
+
+  M5.Lcd.setTextColor(YELLOW, BLACK);
+  M5.Lcd.setTextSize(2.5);
+
   M5.Lcd.print("starting...\n");
   M5.Lcd.print("connecting to wifi...\n");
   connectWiFi();
 }
 
 void loop() {
+  static unsigned long previousMillis = 0;
+  unsigned long currentMillis = millis();
+  unsigned long delta = currentMillis - previousMillis;
   M5.update();
+
+  
+  // M5.lcd.clear();
+  M5.Lcd.setCursor(0, 0);
+
   if (WiFi.status() == WL_CONNECTED) {
-    M5.Lcd.printf("\nConnected!\n");
+    M5.Lcd.printf("Connected! \n");
     M5.Lcd.printf("IP address: %s\n", WiFi.localIP().toString().c_str());
+  }
+
+
+  if (M5.BtnA.wasReleased()) {
+    static bool isOn = false;
+    M5.Lcd.invertDisplay(isOn);
+    isOn = !isOn;
   }
   if (M5.BtnA.pressedFor(1000, 200)) { // If A is pressed for 1 second power off
     M5.Power.powerOFF();
