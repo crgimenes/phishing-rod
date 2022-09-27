a7af648535fe6cb975eec06528fdee57ece918b8
crgimenes <crg@crg.eti.br>
2022-09-27 03:15:41 -0300

    connect to wifi

--- M5Stack/fishing-rod/fishing-rod.ino
+++ M5Stack/fishing-rod/fishing-rod.ino
@@ -1,15 +1,34 @@
 #include <M5Stack.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+#include "secrets.h"
+
+void connectWiFi() {
+  WiFi.begin(SSID, PASSPHRASE);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+}
+
 
 
 void setup() {
   M5.begin();
   M5.Power.begin();
-  M5.Lcd.setTextSize(2);
+  M5.Lcd.setTextSize(3);
   M5.Lcd.print("starting...\n");
+  M5.Lcd.print("connecting to wifi...\n");
+  connectWiFi();
 }
 
 void loop() {
   M5.update();
+  if (WiFi.status() == WL_CONNECTED) {
+    M5.Lcd.printf("\nConnected!\n");
+    M5.Lcd.printf("IP address: %s\n", WiFi.localIP().toString().c_str());
+  }
   if (M5.BtnA.pressedFor(1000, 200)) { // If A is pressed for 1 second power off
     M5.Power.powerOFF();
   }
